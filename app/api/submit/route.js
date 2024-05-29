f99e805ff0ea4815e1ad729ed858bc6f62b84b7b
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-05-29 15:32:02 +0200

    submit route

---
+++ app/api/submit/route.js
@@ -0,0 +1,23 @@
+import OpenAI from "openai";
+import { getChatCompletion, getMovieRecommendation } from "@/utils/utilities";
+
+const openai = new OpenAI(process.env.NEXT_OPENAI_API_KEY);
+
+// POST/api/messages
+export const POST = async request => {
+  try {
+    const { input, query } = await request.json();
+
+    const movie_reco = await getMovieRecommendation(input);
+
+    const chat = await getChatCompletion(movie_reco.content, query);
+    console.log(chat);
+
+    return new Response(JSON.stringify({ movie: movie_reco, chat }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wwrong", { status: 500 });
+  }
+};
