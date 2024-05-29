7e5a16e54ac8ae00fc9a7e8d00c42c944ff3eff3
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-05-29 15:32:11 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    minHeight: {
+      inputHeight: "4rem",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        blueDark: "#000C36",
+        blueBlack: "#3B4877",
+        greenButton: "#51E08A",
+      },
+      width: {
+        small: "30rem",
       },
     },
   },
